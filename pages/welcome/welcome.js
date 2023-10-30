00d0f2a4d218d2f6f4513ff64aecdda7191cc14c
cruiter <cruiter11235@outlook.com>
2023-10-30 10:56:54 +0800

    2.0

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,92 @@
+// pages/welcome/welcome.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    student: {
+      name: '',
+      age: '',
+      address: ''
+    },
+  },
+  submit(event){
+    const {detail} = event;
+    /*
+      detail 返回三个参数
+      1、values: 各表单项的value值
+      2、errors: 各表单项验证后的返回的错误信息数组
+      3、isValidate: 表单是否验证通过的boolean值
+      具体格式示例：
+      detail = {
+         values: {
+             studentName: "",
+             studentAge: "",
+             studentAddress: ""
+         },
+         errors: {
+             studentName: [],
+             studentAge: [],
+             studentAddress: []
+         },
+         isValidate: true
+      }
+    */
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
